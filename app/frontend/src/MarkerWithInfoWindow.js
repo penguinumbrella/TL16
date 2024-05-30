4b825139574633c624982039c02bc88ba31bed5a
F2507 <farouk.sharkia.1@gmail.com>
2024-05-30 14:37:14 -0700

    image imports 2

--- app/frontend/src/MarkerWithInfoWindow.js
+++ app/frontend/src/MarkerWithInfoWindow.js
@@ -4,6 +4,13 @@ import { useState } from 'react';
 
 import * as imagePaths from './imagePaths.js';
 
+import  parkadeIconImage from "./assets/parking-sign-1-2209719147.png"
+import  parkadeIconImagePicked from './assets/parking-sign-1-2209719147_filter.png'
+import  accessibilityIconImage from './/assets/accessibility.png'
+import  accessibilityIconImagePicked from './assets/accessibility_filter.png'
+import loadingZoneIconImage from './assets/loading_zone.png'
+import loadingZoneIconImagePicked from './assets/loading_zone_filter.jpg'
+
 const MarkerWithInfoWindow = ({ position, 
                                 showInfoWindow,
                                 infoWindowShown,
