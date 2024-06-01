14a2e589631c33927e7203a8ce35a8c984da06de
F2507 <farouk.sharkia.1@gmail.com>
2024-05-31 19:31:39 -0700

    frontend and server work together

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,59 +1,37 @@
-// import { useEffect } from 'react';
-// import logo from './logo.svg';
+import { useEffect } from 'react';
+import logo from './logo.svg';
 // import './App.css';
-
-// function App() {
-
-//   useEffect( () => {
-//     const fetchData = async () => {
-//       try {
-//         const res = await fetch('/api');
-//         console.log(await res.json());
-//       } catch (err) {
-//         console.log("Error fetching requested resource")
-//       }
-//     };
-//   fetchData();
-//   }, [])
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React from 'react';
 import Map from './Map.js';
 
+const UBC_lat = 49.262141;
+const UBC_lng = -123.247360;
 
-
-const App = () => {
-  const center = { lat: 49.262141, lng: -123.247360 }; 
+function App() {
+  const center = { lat: UBC_lat, lng: UBC_lng }; 
   const zoom = 14;
 
+  useEffect( () => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('/api');
+        console.log(await res.json());
+      } catch (err) {
+        console.log("Frontend Only")
+      }
+    };
+  fetchData();
+  }, [])
+
   return (
-    <div>
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo}  className="App-logo" alt="logo" /> */}
+        <div>
       <Map center={center} zoom={zoom}/>
     </div>
+      </header>
+    </div>
   );
-};
-
-
+}
 
 export default App;
--- app/frontend/src/Map.js
+++ app/frontend/src/Map.js
@@ -4,11 +4,18 @@ import { GoogleMap, LoadScript} from '@react-google-maps/api';
 
 import MarkerWithInfoWindow from './MarkerWithInfoWindow';
 import mapMarkers from './coordinates.json';
+import initialMarkerData from './markerData.json';
+
 
 
 const Map = ({ center, zoom }) => {
 
+  // hared state between map markers for the infoWindow, only one infoWindow can 
+  // be active at a time and the value of the state is the name of the marker 
   const [activeIndex, setActiveIndex] = useState('');
+
+  // the initial value of markerData is that of the current data in the file
+  const [markerData, setMarkerDataData] = useState(initialMarkerData);
 
   const mapOptions = {
     center: center,
@@ -17,22 +24,50 @@ const Map = ({ center, zoom }) => {
     zoomControl: false,
     disableDoubleClickZoom: true,
     disableDefaultUI: true,
-    scrollwheel: false,
+    scrollwheel: true,
     streetViewControl: false,
   };
 
 
-
+// Default view is parkades
 const [selectedOption, setSelectedOption] = useState('parkades');
 
 
+  // Event handler for picking the category 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
+
+    // Reset the state for the infoWindow
     setActiveIndex("");
   };
 
 
+  // Get the data from the object containing all current marker data
+  const getMarkerData = (option, name) =>{
+    
+    const indexedObject = markerData[option].reduce((acc, obj) => {
+      acc[obj.name] = obj;
+      return acc;}, {});
+      return indexedObject[name].data;
+  }
+
+  
+  // If the file frontend/src/markerData.json is updated and the page is refreshed 
+  // this hook will fetch the new file  and update markerData
+  // 
+  useEffect(() => {
+        fetch('/api/data')
+            .then(response => response.json())
+            .then(data => {
+              setMarkerDataData(JSON.parse(data)); 
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+
+
   return (
+    // Select the category for the map. Default option is parkade
     <LoadScript googleMapsApiKey={process.env.REACT_APP_MAPS_KEY}>
       <div>
         <form>
@@ -49,15 +84,19 @@ const [selectedOption, setSelectedOption] = useState('parkades');
 
     <div style={{ position: 'fixed', top: 0, right: 0, width: '35%', height: '100%' }}>
       <GoogleMap mapContainerStyle={{ width: '100%', height: '100%' }} options={mapOptions} center={center} zoom={zoom}>
-
-
         {
+          // Loop through every parkade / accessibility spot / loading zone (depending on event.target.value) 
+          // and render markers for them
           mapMarkers[selectedOption].map(item => {
             return (<MarkerWithInfoWindow position={item.location} 
                                           content={item.name}  
                                           infoWindowShown={activeIndex === item.name}  
                                           showInfoWindow={() => setActiveIndex(item.name)} 
-                                          exit={() => setActiveIndex('')} iconImage={selectedOption}  />)
+                                          exit={() => setActiveIndex('')} iconImage={selectedOption} 
+
+                                          // 5 is just just a placeholder 
+                                          data={getMarkerData(selectedOption, item.name)}
+                                          />)
           })
     
         }
