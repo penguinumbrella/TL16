21a7ba2980b4c553f58f275cbadbe4bd9d04d564
aayushbehl <aayush.behl@ubc.ca>
2024-06-14 01:10:28 -0700

    Debugging tooltips

--- app/frontend/src/components/diagrams/LineGraph/LineGraphComponent.js
+++ app/frontend/src/components/diagrams/LineGraph/LineGraphComponent.js
@@ -7,7 +7,7 @@ const LineGraphComponent = ({ data, width, height, title='', customToolTip }) =>
         <LineChart data={data}>
             <XAxis dataKey={"name"} style={{fontSize: "10px"}}/>
             <YAxis dataKey={"value"} style={{fontSize: "10px"}}/>
-            <Tooltip content={customToolTip}/>
+            <Tooltip content={<customToolTip/>}/>
             <Legend/>
             <Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{ r: 4 }} />
         </LineChart>
