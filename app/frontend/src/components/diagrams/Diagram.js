f8429dbbbb89708ca9ee9cd254660c66d51baa1d
AdinMauer <mauer4@gmail.com>
2024-06-13 17:55:52 -0700

    Pie chart modified to look like figme. TBD: - Pass capacity as parameter to chart - show accuarte percentage around chart - start "available" from top, and make available color gray

--- app/frontend/src/components/diagrams/Diagram.js
+++ app/frontend/src/components/diagrams/Diagram.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 const Diagram = ({type, width, height, title='', query=''}) => {
 
   const [diagData, setDiagData] = useState([]);
+  const [occupancyPercentage, setOccupancyPercentage] = useState('');
 
   const DATA = [
         { name: 'Group A', value: 400 },
@@ -23,25 +24,38 @@ const Diagram = ({type, width, height, title='', query=''}) => {
     return DATA;
   }
 
-  let toRender;
-
   useEffect(() => {
     const getData = async () => {
       const data = (await axios.get(`/executeQuery?query=${query}`)).data;
       console.log(data[0]['Capacity'])
+      const capacity = data[0]['Capacity'];
+      const occupied = data[0]['Vehicles'];
+      const occupancyPercentage = ((occupied / capacity) * 100).toFixed(1);
       setDiagData([
         {name: 'Available', value: data[0]['Capacity'] - data[0]['Vehicles']},
         {name: 'Occupied', value: data[0]['Vehicles']}
       ]);
+      setOccupancyPercentage(`${occupancyPercentage}%`);
     }
     getData();
   }, [])
-  
+
+  let toRender;
+
   switch(type) {
     case 'PIE': 
         toRender = <>
-            <PieChartComponent data={diagData} colors={COLORS} height={height} width={width} title={title}></PieChartComponent>
-        </>
+            <PieChartComponent 
+              data={diagData} 
+              colors={COLORS} 
+              height={height} 
+              width={width} 
+              title={title} 
+              innerRadius={50}
+              outerRadius={57.5}
+              percentageCenter={occupancyPercentage}>
+            </PieChartComponent>
+            </>
         break;
     case 'LINE':
         toRender = <>
--- app/frontend/src/components/diagrams/PieChart/PieChartComponent.js
+++ app/frontend/src/components/diagrams/PieChart/PieChartComponent.js
@@ -1,43 +1,93 @@
 import React, { useState } from 'react';
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer, Legend, Label } from 'recharts';
 
-const PieChartComponent = ({data, colors, width, height, title=''}) => {
+const PieChartComponent = ({data, colors, width, height, title='', innerRadius, outerRadius , percentageCenter}) => {
 
   const [ activeIndex, setActiveIndex ] = useState(-1);
 
   const RADIAN = Math.PI / 180;
+
   const renderCustomizedLabel = ({
     cx, cy, midAngle, innerRadius, outerRadius, percent, index,
     }) => {
+        
+    if (index !== activeIndex) {
+      return null;
+    }
+      
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 2) * cos;
+    const sy = cy + (outerRadius + 2) * sin;
+    const mx = cx + (outerRadius + 6) * cos;
+    const my = cy + (outerRadius + 6) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 9;
+    const ey = my;
+    const textAnchor = cos >= 0 ? 'start' : 'end';
+
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
-    let toDisplay;
-    if (index != activeIndex)
-      toDisplay = data[index].value;
-    else toDisplay = `${percent.toFixed(1)*100}%`;
+
+    let toDisplayName;
+    let toDisplayValue;
+    toDisplayName = data[index].name;
+    toDisplayValue = `${percent.toFixed(1)*100}%`;
+
     return (
-      <text x={x} y={y} fill="white" textAnchor="middle" dominantBaseline="central" fontSize={`${outerRadius/6}px`}>
-        {`${toDisplay}`}
-      </text>
+      <g>
+        <path 
+          d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} 
+          stroke={colors[index % colors.length]} 
+          fill="none" 
+        />
+        <circle cx={ex} cy={ey} r={2} fill={colors[index % colors.length]} stroke="none" />
+        <text x={ex + (cos >= 0 ? 1 : -1) * 6} y={ey} dy={5} textAnchor={textAnchor} fill="#999" fontSize={16}>
+        {`${toDisplayValue}`}
+        </text>
+      </g>
     );
   };
 
   // https://recharts.org/en-US/examples/CustomActiveShapePieChart
   const renderActiveShape = (props) => {
     const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
+    
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 5) * cos;
+    const my = cy + (outerRadius + 5) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? 'start' : 'end';
+
     return (
       <g>
-        <text></text>
         <Sector
           cx={cx}
           cy={cy}
-          innerRadius={innerRadius + 6}
-          outerRadius={outerRadius + 10}
+          innerRadius={innerRadius - 1}
+          outerRadius={outerRadius + 1}
           startAngle={startAngle}
           endAngle={endAngle}
           fill={fill}
+          stroke="#fff" // Add stroke to maintain white border
+          strokeWidth={1}
         />
+        {/* <path 
+          d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} 
+          stroke={fill} 
+          fill="none" 
+        /> */}
+        {/* <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">
+          {'text'}
+        </text>
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+          {`(${(percent * 100).toFixed(1)}%)`}
+        </text> */}
       </g>
     );
   };
@@ -45,16 +95,32 @@ const PieChartComponent = ({data, colors, width, height, title=''}) => {
   return (
     <ResponsiveContainer width={width} height={height} style={{display: 'flex', flexDirection: 'column'}}>
       <PieChart width={'100%'} height={'100%'}>
-        <Pie data={data} label={renderCustomizedLabel} labelLine={false} activeIndex={activeIndex}
-         activeShape={renderActiveShape} onMouseEnter={(_, index) => setActiveIndex(index)}
-         onMouseLeave={() => setActiveIndex(-1)}>
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-            ))}
+        <Pie 
+          data={data} 
+          label={renderCustomizedLabel} 
+          labelLine={false} 
+          activeIndex={activeIndex}
+          activeShape={renderActiveShape} 
+          onMouseEnter={(_, index) => setActiveIndex(index)}
+          onMouseLeave={() => setActiveIndex(-1)}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+          ))}
+        
         </Pie>
         <Legend></Legend>
+        <text x={width / 2} y={height / 2 - 15} textAnchor="middle" fill="#888" fontSize={15} style={{ textAlign: 'center' }}>
+          {title}
+        </text>
+        {/* Percentage Occupancy */}
+        <text x={width / 2} y={height / 2 + 5} dy={8} textAnchor="middle" fill="#FFF" fontSize={22}>
+          {percentageCenter}
+        </text>
       </PieChart>
-      <text style={{textAlign: 'center'}}>{title}</text>
+      
     </ResponsiveContainer>
   )
 }
