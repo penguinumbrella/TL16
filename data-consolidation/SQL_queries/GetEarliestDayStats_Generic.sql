feac08c13696810876896d72efff8cd6d45994e5
mauer4 <mauer4@gmail.com>
2024-07-04 17:44:11 -0700

    Push sql queries

---
+++ data-consolidation/SQL_queries/GetEarliestDayStats_Generic.sql
@@ -0,0 +1,54 @@
+-- Create a temporary table to hold the data for the earliest day
+WITH earliest_day_data AS (
+    SELECT
+        zone_id,
+        DATEADD(SECOND, TimestampUnix, '1970-01-01') AS date,
+        Vehicles,
+        TimestampUnix
+    FROM 
+        [Parking].[dbo].[RoseGardenParkade_Occupancy]
+    WHERE
+        CAST(DATEADD(SECOND, TimestampUnix, '1970-01-01') AS DATE) = (
+            SELECT MIN(CAST(DATEADD(SECOND, TimestampUnix, '1970-01-01') AS DATE))
+            FROM [Parking].[dbo].[RoseGardenParkade_Occupancy]
+        )
+),
+-- Subquery to calculate average values
+average_data AS (
+    SELECT
+        CAST(date AS DATE) AS date,
+        zone_id,
+        AVG(Vehicles) AS average_value
+    FROM
+        earliest_day_data
+    GROUP BY
+        CAST(date AS DATE),
+        zone_id
+),
+-- Subquery to find peak values and their corresponding timestamps
+peak_data AS (
+    SELECT
+        CAST(date AS DATE) AS date,
+        zone_id,
+        Vehicles AS peak_value,
+        TimestampUnix AS peak_unix_timestamp,
+        ROW_NUMBER() OVER (PARTITION BY zone_id, CAST(date AS DATE) ORDER BY Vehicles DESC, TimestampUnix ASC) AS rn
+    FROM
+        earliest_day_data
+)
+-- Insert the aggregated data into 'Daily_Occupancy_Stats' table
+INSERT INTO [Parking].[dbo].[Daily_Occupancy_Stats] (date, zone_id, average_occupancy, peak_occupancy, peak_occupancy_time)
+SELECT 
+    a.date,
+    a.zone_id,
+    a.average_value,
+    p.peak_value,
+    p.peak_unix_timestamp
+FROM 
+    average_data a
+JOIN 
+    (SELECT date, zone_id, peak_value, peak_unix_timestamp
+     FROM peak_data
+     WHERE rn = 1) p
+ON 
+    a.date = p.date AND a.zone_id = p.zone_id;
